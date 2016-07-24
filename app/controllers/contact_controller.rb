b39d6b3c6fd2d13729af27f4cfb85be2eeef952f
vanurox <varunrocks27.sharma@gmail.com>
2016-07-24 16:20:21 +0530

    updated some contents

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,5 @@
 class ContactController < ApplicationController
   def help
+  	@numbers = [9915296866,001,98190,98140]
   end
 end
--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -5,6 +5,8 @@ class HelloController < ApplicationController
 
 	def solution
 		# render "welcome"
+		# redirect_to :controller=>"contact",:action=>"help"
+		redirect_to :action=>"welcome"
 	end
 
 	def contact
