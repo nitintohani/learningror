49805ffc95ebcb5c9647205d9a11ee47f1f76c1c
vanurox <varunrocks27.sharma@gmail.com>
2016-08-04 18:43:53 +0530

    form updated with rails helper and valdiations added in model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,29 @@
 class UsersController < ApplicationController
   def new
+
   end
   def index
   end
   def create
-	@page_title = "New User"
-	@email = params[:email]
-	@password = params[:password]
-	@user = User.new
-	@user.email = @email
-	@user.password = @password
-	if @user.save
-		@msg = "User saved successfully"
-		render 'new'
-	end
+	# @page_title = "New User"
+	# @email = params[:email]
+	# @password = params[:password]
+	# @user = User.new
+	# @user.email = @email
+	# @user.password = @password
+	# if @user.save
+	# 	@msg = "User saved successfully"
+	# 	render 'new'
+	# end
+  @user = User.new(user_params)
+  if @user.valid?
+   @user.save
+    @msg = "User saved successfully"
+  else
+    @errors = @user.errors.full_messages    
+  end
+    render 'new'
+
   end
 
   def destroy 
@@ -26,5 +36,9 @@ class UsersController < ApplicationController
 
   def show
   	@users = User.all
+  end
+
+  def user_params
+    return params.require(:user).permit(:email,:password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-	
+	validates_presence_of :email
+	validates_uniqueness_of :email
+	validates_presence_of :password
 end
